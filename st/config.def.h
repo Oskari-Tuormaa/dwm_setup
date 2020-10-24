86f0f0534b7f544eae356ae517d36332dc191afd
Oskari Tuormaa <oskaritu@gmail.com>
2020-10-24 21:16:12 +0200

    Changed to gruvbox colorscheme

--- st/config.def.h
+++ st/config.def.h
@@ -96,40 +96,32 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-
-  /* 8 normal colors */
-  [0] = "#1c1c1c", /* black   */
-  [1] = "#af5f5f", /* red     */
-  [2] = "#5f875f", /* green   */
-  [3] = "#87875f", /* yellow  */
-  [4] = "#5f87af", /* blue    */
-  [5] = "#5f5f87", /* magenta */
-  [6] = "#5f8787", /* cyan    */
-  [7] = "#6c6c6c", /* white   */
-
-  /* 8 bright colors */
-  [8]  = "#444444", /* black   */
-  [9]  = "#ff8700", /* red     */
-  [10] = "#87af87", /* green   */
-  [11] = "#ffffaf", /* yellow  */
-  [12] = "#8fafd7", /* blue    */
-  [13] = "#8787af", /* magenta */
-  [14] = "#5fafaf", /* cyan    */
-  [15] = "#ffffff", /* white   */
-
-  /* special colors */
-  [256] = "#262626", /* background */
-  [257] = "#bcbcbc", /* foreground */
+	"#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	"#cc241d",
+	"#98971a",
+	"#d79921",
+	"#458588",
+	"#b16286",
+	"#689d6a",
+	"#a89984",
+	"#928374",
+	"#fb4934",
+	"#b8bb26",
+	"#fabd2f",
+	"#83a598",
+	"#d3869b",
+	"#8ec07c",
+	"#ebdbb2",
 };
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor
  */
-unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
+unsigned int defaultfg = 15;
+unsigned int defaultbg = 0;
 static unsigned int defaultcs = 15;
-static unsigned int defaultrcs = 257;
+static unsigned int defaultrcs = 15;
 
 /*
  * Default shape of cursor
